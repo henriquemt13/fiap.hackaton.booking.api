0b1de04a65af00153859a3abb92a76128e01bcb4
Henrique Matos <47531611+henriquemt13@users.noreply.github.com>
2024-03-14 08:31:01 -0300

    chore: add error logs

--- hackaton.booking.api/src/main/java/com/hackaton/booking/api/filter/ControllerAdvisor.java
+++ hackaton.booking.api/src/main/java/com/hackaton/booking/api/filter/ControllerAdvisor.java
@@ -3,6 +3,7 @@ package com.hackaton.booking.api.filter;
 import com.hackaton.booking.api.exceptions.BadRequestException;
 import com.hackaton.booking.api.exceptions.NotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,40 +15,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
+@Slf4j
 public class ControllerAdvisor {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity handleNotFoundException(NotFoundException ex, WebRequest request) {
+        log.error(ex.getLocalizedMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+              .body(ex.getMessage());
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity handleBadRequestException(BadRequestException ex, WebRequest request) {
+        log.error(ex.getLocalizedMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ex.getMessage());
+              .body(ex.getMessage());
     }
 
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public ResponseEntity handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                .body(ex.getMessage());
-//    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handleIllegalArgumentException(IllegalArgumentException ex,
+          WebRequest request) {
+        log.error(ex.getLocalizedMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+              .body(ex.getMessage());
+    }
 
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity handleContraintViolationException(ConstraintViolationException ex, WebRequest request) {
+    public ResponseEntity handleContraintViolationException(ConstraintViolationException ex,
+          WebRequest request) {
         List<String> errors = new ArrayList<>();
         ex.getConstraintViolations().forEach(e -> errors.add(
-                String.format("%s", e.getMessage())));
+              String.format("%s", e.getMessage())));
+        log.error(errors.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+          WebRequest request) {
         List<String> errors = new ArrayList<>();
         ex.getFieldErrors().forEach(e -> errors.add(
-                String.format("%s: %s (%s)", e.getField(), e.getRejectedValue(), e.getDefaultMessage())));
+              String.format("%s: %s (%s)", e.getField(), e.getRejectedValue(),
+                    e.getDefaultMessage())));
+        log.error(errors.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
